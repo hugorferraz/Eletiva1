58699bfebab44e53aaea721a38efeaf281c6e27f
hugorferraz <103072450+hugorferraz@users.noreply.github.com>
2026-08-21 14:47:35 -0300

    Delete aula1 directory

--- aula1/resposta2.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $valor1 = $_POST["valor1"];
-    $valor2 = $_POST["valor2"];
-    $subt = $valor1 - $valor2;
-    echo "O valor da subtração é: $subt";
--- aula1/resposta3.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $valor1 = $_POST["valor1"];
-    $valor2 = $_POST["valor2"];
-    $mult = $valor1 * $valor2;
-    echo "O valor da multiplicação é: $mult";
