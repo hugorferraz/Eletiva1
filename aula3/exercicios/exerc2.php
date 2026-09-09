9e594e5bc7b941f0c14964d25ab1fc0cbf0ed672
Hugo <fhugoferraz@gmail.com>
2026-09-09 07:53:18 -0300

    exercício2 - OK

--- aula3/exercicios/exerc2.php
+++ aula3/exercicios/exerc2.php
@@ -3,30 +3,31 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 1 - Funções</title>
+    <title>Exercício 2 - Funções</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
     <div class="container py-3">
-        <h1>Exercício 1 - Funções</h1>
-            <form method="post" action="exerc1.php">
+        <h1>Exercício 2 - Funções</h1>
+            <form method="post" action="exerc2.php">
             <div class="mb-3">
-                <label for="nome" class="form-label">Digite o seu nome: </label>
-                <input type="text" id="nome" name="nome" class="form-control" required="">
+                <label for="palavra" class="form-label">Digite uma palavra: </label>
+                <input type="text" id="palavra" name="palavra" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
             </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    
         <?php
             if ($_POST){
-                $nome = $_POST['nome'];
+                $palavra = $_POST['palavra'];
 
-                if ($nome != ""){
-                    $qtdLetras = strlen($nome);
-                    echo "<p>Quantidade de letras: $qtdLetras</p>";
+                if ($palavra != ""){
+                    echo "<p> Maiúsculo: " . strtoupper($palavra) . "</p>";
+                    echo "<p> Minúsculo: " . strtolower ($palavra) . "</p>";
                 }
             }
-        ?>
+        ?>  
     </div>
 </body>
 </html>
