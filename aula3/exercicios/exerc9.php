7f1e6d77c29387d339c05049fb3c1ccc3d557b9c
Hugo <fhugoferraz@gmail.com>
2026-09-09 16:44:06 -0300

    exercicio9 - OK

--- aula3/exercicios/exerc9.php
+++ aula3/exercicios/exerc9.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 8 - Funções</title>
+    <title>Exercício 9 - Funções</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
     <div class="container py-3">
-        <h1>Exercício 8 - Função preg_match_all()</h1>
-            <form method="post" action="exerc8.php">
+        <h1>Exercício 9 - Função trim() e str_replace()</h1>
+            <form method="post" action="exerc9.php">
             <div class="mb-3">
                 <label for="frase" class="form-label">Digite uma frase: </label>
                 <input type="text" id="frase" name="frase" class="form-control" required="">
@@ -20,13 +20,15 @@
     
         <?php
             if ($_POST){
-                $frase = $_POST['frase'] ?? "";
-                if ($frase != ""){
-                    // u habilita acento e i ignora diferença entre maiúscula e minúscula
-                    $totalVogais = preg_match_all('/[aeiouáéíóúâêîôûãõà]/ui', $frase);
+                $fraseOriginal = $_POST ['frase'] ?? "";
 
-                    echo "<p>Frase digitada: $frase </p>";
-                    echo "<p> Total de vogais na frase: $totalVogais </p>";
+                if ($frase != ""){
+                    $fraseSemEspacosIniFin = trim($fraseOriginal);
+                    $fraseSemNenhumEspaco = str_replace(' ', '', $fraseSemNenhumEspaco);
+
+                    echo "<p> Frase original: $fraseOriginal </p>";
+                    echo "<p> Frase sem espaços iniciais e finais $fraseSemEspacosIniFin </p>";
+                    echo "<p> Frase sem nenhum espaço $fraseSemNenhumEspaco </p>";
                 }
             }
         ?>
